076e091b72d6e29f14bf2912d06f14cc4e9612a9
parasmehta70156 <paras8684898906@gmail.com>
2026-02-16 22:44:37 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
 android {
     namespace = "com.paras.generatedapp"
     compileSdk = 34
@@ -24,10 +19,13 @@ android {
     buildFeatures {
         viewBinding = true
     }
-}
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
