02018b03945b74b28ec2db194c7c22c9bc18f8ec
parasmehta70156 <paras8684898906@gmail.com>
2026-02-16 22:50:05 +0530

    fix gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
     namespace = "com.paras.generatedapp"
     compileSdk = 34
@@ -28,4 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
